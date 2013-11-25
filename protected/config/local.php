9a6dc7e8ca85e123b70460a123b3df955047091e
senhong <senhongwang@gmail.com>
2013-11-25 17:36:47 +0800

    add previous ignored files

---
+++ protected/config/local.php
@@ -0,0 +1,35 @@
+<?php
+
+return array(
+    'params'=>array(
+//        'home_url' => 'http://gigadb.org', // Where top level link goes to
+        'home_url'=>'http://192.168.171.46',
+        // Used in the contact page
+        'adminEmail' => 'database@gigasciencejournal.com',
+//          'adminEmail' => 'senhongwang@gmail.com', 
+        
+        // This is used as the sender of emails
+        'app_email_name' => 'Giga DB',
+        'app_email' => 'database@gigasciencejournal.com',
+//        'app_email' => 'senhongwang@gmail.com',
+        'email_prefix' => '[Giga DB] ', // put at the begining of the subject line
+
+        'support_email' => 'database@gigasciencejournal.com',
+
+        // Notified when a new user signs up
+        'notify_email' => 'database@gigasciencejournal.com',
+
+        // Cogini
+        'recaptcha_publickey' => '6LdkzgsAAAAAAGFJ5GdJyXf4JvFqKJe5N0-F3IRt',
+        'recaptcha_privatekey' => '6LdkzgsAAAAAAD6w-JyWXLfN1260aeRNl33I-JH9',
+        "google_analytics_profile" => "UA-40438882-1",
+ 
+        // Sphinx params
+        'sphinx_servername' => '127.0.0.1',
+        'sphinx_port' => 9312,
+
+        // MC api
+        'mc_apikey' => 'a6f0c4189a9407a2758ad9f53f22dc67-us5',
+        'mc_listID' => '4be098275c',
+    ),
+);
